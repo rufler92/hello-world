2f05c71fe498d84aba08ae16f3e6a62df9a88160
rufler92 <kamilmysza92@interia.pl>
2022-03-02 20:36:51 +0100

    first commit

---
+++ src/main/java/helloworld/Inicjaly.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Inicjaly {
+    public static void main(String[] args) {
+        char k = 'K';
+        char m = 'M';
+        System.out.println(k + "." + m + "." );
+    }
+}
