8e00ae996716433536968e664bb5a07eb75eea0b
rufler92 <kamilmysza92@interia.pl>
2022-03-02 20:29:04 +0100

    first commit

--- src/main/java/helloworld/LiczbaMiesiecyWRoku.java
+++ src/main/java/helloworld/LiczbaMiesiecyWRoku.java
@@ -2,6 +2,7 @@ package helloworld;
 
 public class LiczbaMiesiecyWRoku {
     public static void main(String[] args) {
-
+        final int LICZBA_MIESIECY_W_ROKU = 12;
+        System.out.println("LiczbaMiesiecyWRoku:" + LICZBA_MIESIECY_W_ROKU);
     }
 }
