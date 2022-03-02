08c4995a20aaaa10bfa724e640978d8b6df7edbf
rufler92 <kamilmysza92@interia.pl>
2022-03-02 16:12:43 +0100

    first commit

---
+++ src/main/java/helloworld/ObwodTrojkata.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class ObwodTrojkata {
+    public static void main(String[] args) {
+        int x = 2, y = 3, z = 4;
+        System.out.println( x + y + z );
+    }
+}
