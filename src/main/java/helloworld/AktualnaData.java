0274d080f2f8aee601744f5d65f2a5884e6d12a8
rufler92 <kamilmysza92@interia.pl>
2022-03-02 16:24:41 +0100

    first commit

--- src/main/java/helloworld/AktualnaData.java
+++ src/main/java/helloworld/AktualnaData.java
@@ -2,7 +2,10 @@ package helloworld;
 
 public class AktualnaData {
     public static void main(String[] args) {
-
+    int aktualnyDzien = 2;
+    int miesiac = 3;
+    int rok = 2022;
+        System.out.println( aktualnyDzien + miesiac + rok );
     }
 }
 
