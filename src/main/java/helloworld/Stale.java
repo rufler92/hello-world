90f1e342f5bc50dd8631e2b764aa85a0da1aa643
rufler92 <kamilmysza92@interia.pl>
2022-03-02 15:35:37 +0100

    first commit

--- src/main/java/helloworld/Stale.java
+++ src/main/java/helloworld/Stale.java
@@ -5,6 +5,10 @@ import java.util.OptionalDouble;
 
 public class Stale {
     public static void main(String[] args) {
+        final double PI = 3.14;
+        final int LICZBA_DNI_W_TYGODNIU = 7;
 
+        int promien = 20;
+        double PoleKola = PI * promien * promien;
     }
 }
