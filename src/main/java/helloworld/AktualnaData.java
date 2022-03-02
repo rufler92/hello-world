d972fc60fd0046c0dcd902a015a4097487a3239d
rufler92 <kamilmysza92@interia.pl>
2022-03-02 16:19:50 +0100

    first commit

---
+++ src/main/java/helloworld/AktualnaData.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class AktualnaData {
+    public static void main(String[] args) {
+
+    }
+}
+
--- src/main/java/helloworld/TrzyZmienne.java
+++ src/main/java/helloworld/TrzyZmienne.java
@@ -3,6 +3,6 @@ package helloworld;
 public class TrzyZmienne {
     public static void main(String[] args) {
     int x = 2, y = 4;
-        System.out.println(x + y );
+        System.out.println( x + y );
     }
 }
